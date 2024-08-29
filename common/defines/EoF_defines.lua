6c7771f1f3fe35f61d7f4d5b2bd5458c4518ce83
Tassen1221 <by200212@gmail.com>
2024-08-29 15:22:24 -0700

    Small Bag'o Balance
    -WNG Generals better
    -Armour cares more about the highest battalion
    -Divisions split less of their attack on the main target
    -Mob speed penalty for higher army professionalism reduced
    -JER Naval Designers Buffed
    -Stability gives less positive effects
    -CVCAS comes in airwings of 100
    -Bomb locks allow port strike
    -EQC Bugfix

--- common/defines/EoF_defines.lua
+++ common/defines/EoF_defines.lua
@@ -94,10 +94,10 @@ NDefines.NMilitary.PIERCING_THRESHOLD_DAMAGE_VALUES = {		-- 0 armor will always 
 		0.10,
 	}
 
-NDefines.NMilitary.ARMOR_VS_AVERAGE = 0.3			-- how to weight in highest armor & pen vs the division average
+NDefines.NMilitary.ARMOR_VS_AVERAGE = 0.4			-- how to weight in highest armor & pen vs the division average
 NDefines.NMilitary.PEN_VS_AVERAGE = 0.3
 
-NDefines.NMilitary.DAMAGE_SPLIT_ON_FIRST_TARGET = 0.6		--% of damage dealt to the first target in a combat. The rest will be split amongst subsequent targets. Modifiers can affect this up to a maximum of 0.9. That value must not be exposed as a define.
+NDefines.NMilitary.DAMAGE_SPLIT_ON_FIRST_TARGET = 0.35		--% of damage dealt to the first target in a combat. The rest will be split amongst subsequent targets. Modifiers can affect this up to a maximum of 0.9. That value must not be exposed as a define.
 
 NDefines.NCharacter.POLITICAL_ADVISOR_SLOTS_IN_MENU = 6		--For Alert manager to count the number of Political Advisor Slots in the UI
 
