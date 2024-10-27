6664b663f5b0fc87f25f065bd534edc6554c520a
Tassen1221 <by200212@gmail.com>
2024-10-27 12:09:39 -0700

    Doctrine Rebalances
    -MW gets +10% brk for all armour
    -MW left gets 10 more org for all mot/mech guns
    -MW right-right gets 5 more org for tanks and 10 more org for motmech
    -SF and GBP lose 5 org for mot/mech
    -MA right loses some resistance and increases combat width for all armour by 1
    -Deep Battle army spirit increases chance of masterful blitz by 100% if preferred tactic
    -Entrenchment gives 1.5% per value down from 2%
    -Guerilla tactics requires lower than 10% hardness

--- common/defines/EoF_defines.lua
+++ common/defines/EoF_defines.lua
@@ -39,6 +39,8 @@ NDefines.NCountry.MAX_WAR_SUPPORT = 1.0
 NDefines.NMilitary.UNIT_LEADER_USE_NONLINEAR_XP_GAIN = false	-- Whether unit leader XP gain is scaled by 1/<nr_of_traits>
 NDefines.NMilitary.MAX_NUM_TRAITS = 8				-- cant have more, -1 to disable
 
+NDefines.NMilitary.DIG_IN_FACTOR = 0.015			-- bonus factor for each dug-in level
+
 NDefines.NMilitary.MAX_DIVISION_BRIGADE_WIDTH = 6		-- Max width of regiments in division designer.
 NDefines.NMilitary.MAX_DIVISION_BRIGADE_HEIGHT = 5		-- Max height of regiments in division designer.
 NDefines.NMilitary.MIN_DIVISION_BRIGADE_HEIGHT = 4		-- Min height of regiments in division designer.
